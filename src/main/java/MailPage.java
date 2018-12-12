efc163aad54cff21267eb9eb9cdcc2d7ef1f5f3a
Efim <woz.efim@gmail.com>
2018-12-12 23:53:22 +0200

    StaleElementReferenceException fix

--- src/main/java/MailPage.java
+++ src/main/java/MailPage.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
@@ -24,7 +25,9 @@ public class MailPage {
     public void clickToCheckbox() {
         List<WebElement> messages = listMessages();
         for (WebElement element : messages) {
-            WebElement checkbox = element.findElement(By.xpath("//div[@class='oZ-jc T-Jo J-J5-Ji ']"));
+            WebElement checkbox = element.findElement(By.xpath(".//div[@dir='ltr' and @role='checkbox']"));
+            ////div[@class='oZ-jc T-Jo J-J5-Ji '] - checkbox xpath v1
+            //.//div[@dir='ltr' and @role='checkbox']
             checkbox.click();
 
         }
@@ -36,6 +39,30 @@ public class MailPage {
     public void deleteMessages() {
         WebElement deleteicon = driver.findElement(By.xpath("//div[@class='asa']"));
         deleteicon.click();
+    }
+
+    public void verifyNewMessages(){
+        List<WebElement> newmessages = listMessages();
+        while (newmessages.size() != 0){
+            retryingFindClick();
+            //clickToCheckbox();
+            deleteMessages();
+        }
+    }
+
+    public boolean retryingFindClick() {
+        boolean result = false;
+        int attempts = 0;
+        while(attempts < 2) {
+            try {
+                driver.findElement(By.xpath(".//div[@dir='ltr' and @role='checkbox']")).click();
+                result = true;
+                break;
+            } catch(StaleElementReferenceException e) {
+            }
+            attempts++;
+        }
+        return result;
     }
 
 
