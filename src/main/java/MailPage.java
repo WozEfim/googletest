cb0b8cd4dad0f0707b82fb3091500e475879fae1
woz.efim@gmail.com <r3t866mu302>
2018-12-12 16:00:27 +0200

    MailPage new class

--- src/main/java/MailPage.java
+++ src/main/java/MailPage.java
@@ -1,5 +1,9 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
 
 public class MailPage {
 
@@ -9,19 +13,31 @@ public class MailPage {
         this.driver = driver;
     }
 
+    // поместил вебелементы писем на странице с сообщениями
+    public List<WebElement> listMessages() {
+        return driver.findElements(By.xpath(".//tr[@jsaction='bjyjJe:NOSeAe;pInidd:NOSeAe;']"));
 
-    private By checkAllButton = By.xpath("//span[@class='T-Jo J-J5-Ji']");
-    private By deleteButton = By.xpath("//div[@class='asa']");
-
-    public void  clickCheckAllButton() {
-        driver.findElement(checkAllButton).click();
 
     }
 
-    public void clickDeleteButton() {
-        driver.findElement(deleteButton).click();
+    // прошелся по каждому сообщению кликнув на чекбокс выделенния
+    public void clickToCheckbox() {
+        List<WebElement> messages = listMessages();
+        for (WebElement element : messages) {
+            WebElement checkbox = element.findElement(By.xpath("//div[@class='oZ-jc T-Jo J-J5-Ji ']"));
+            checkbox.click();
+
+        }
+
 
     }
+
+    // удалил ранее выделенные сообщения
+    public void deleteMessages() {
+        WebElement deleteicon = driver.findElement(By.xpath("//div[@class='asa']"));
+        deleteicon.click();
+    }
+
 
 
 }
--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,6 +1,5 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.concurrent.TimeUnit;
 
@@ -12,11 +11,13 @@ public class MainClass {
     public static void main(String[] args) {
         // for MacOs geckodriver
         //System.setProperty("webdriver.gecko.driver", "/Users/efim/chromedr/geckodriver");
+
         // for Win10 geckodriver
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\qa\\chromedriver\\chromedriver.exe");
 
 
         driver = new ChromeDriver();
+        driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         String url = "https://accounts.google.com/ServiceLogin/identifier?service=mail&passive=true&rm=false&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&ss=1&scc=1&ltmpl=default&ltmplcache=2&emr=1&osid=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin";
         driver.get(url);
@@ -30,8 +31,12 @@ public class MainClass {
         signinpagewithpassword.lastSteptypeCredits("R3t866mu302");
 
         MailPage mailpage = new MailPage(driver);
-        mailpage.clickCheckAllButton();
-        mailpage.clickDeleteButton();
+        mailpage.clickToCheckbox();
+        mailpage.deleteMessages();
+        mailpage.verifyMessages();
+
+
+
 
 
     }
